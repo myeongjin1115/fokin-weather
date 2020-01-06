8965fb31645f593c6715e2c66e1eb77b83d1f132
Myeongjin Kim <myeongjin1115@gmail.com>
2020-01-06 16:51:59 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
 });
